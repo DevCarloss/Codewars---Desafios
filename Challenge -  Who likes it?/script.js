dc88e870aa8fd83329d5527bbe32708c02bab242
Carlos <112131250+DevCarloss@users.noreply.github.com>
2023-09-08 13:19:25 +0000

    Segundo Commit

--- Challenge -  Who likes it?/script.js
+++ Challenge -  Who likes it?/script.js
@@ -9,3 +9,27 @@
 // ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
 // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+const names = ["Alex", "Jacob", "Mark", "Max"]
+
+function who_likes_it(){
+    switch(names.length){
+        case 0:
+            return console.log("No One Like This")
+            break
+        case 1:
+            return console.log(names[0] + ' Likes This')
+            break
+        case 2:
+            return console.log(names[0] + ' and ' + names[1] + ' Like This')
+            break
+        case 3:
+            return console.log(names[0] + ', ' + names[1] + ' and ' + names[2] +  ' Like This')
+            break
+        default:
+            return console.log(names[0] + ', ' + names[1] + ' and ' + parseInt(names.length - 2) +  ' Others Like This')
+    }
+}
+
+who_likes_it()
+
